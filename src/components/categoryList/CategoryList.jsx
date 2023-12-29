62bcb632faa9619e88954cb046a118a9bd2fc2f2
Ömer Ulusal <96357374+omerulusal@users.noreply.github.com>
2023-12-29 12:34:25 +0300

    CategoryList and Featured design

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 import "./_categoryList.scss"
-const CategoryList = () => {
+const CategoryList = async () => {
     return (
-        <div>CategoryList</div>
-    )
-}
+        <div className="categoryList">
+            <h1 className="title">Popular Categories</h1>
+            <div className="categories">
+                <Link href="/blog?cat=style" className="style">
+                    <Image
+                        src={"/style.png"}
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="Categoryimage"
+                    />
+                    style
+                </Link>
+                <Link href="/blog?cat=style" className="fashion">
+                    <Image
+                        src={"/style.png"}
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="Categoryimage"
+                    />
+                    fashion
+                </Link>
+                <Link href="/blog?cat=style" className="food">
+                    <Image
+                        src={"/style.png"}
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="Categoryimage"
+                    />
+                    food
+                </Link>
+                <Link href="/blog?cat=style" className="travel">
+                    <Image
+                        src={"/style.png"}
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="Categoryimage"
+                    />
+                    Travel
+                </Link>
+                <Link href="/blog?cat=style" className="culture">
+                    <Image
+                        src={"/style.png"}
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="Categoryimage"
+                    />
+                    culture
+                </Link>
+                <Link href="/blog?cat=style" className="coding">
+                    <Image
+                        src={"/style.png"}
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="Categoryimage"
+                    />
+                    coding
+                </Link>
+            </div>
+        </div>
+    );
+};
 
 export default CategoryList
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import "./_featured.scss"
+import Image from "next/image"
 const Featured = () => {
     return (
-        <div>Featured</div>
+        <div className='featured'>
+            <h1 className='title'>
+                <b>Hey, omer here!</b>
+                <br />
+                Discover my stories and creative ideas.
+            </h1>
+            <div className="post">
+                <div className="imgContainer">
+                    <Image className='image' src={"/p1.jpeg"} alt="img" width={420} height={400} />
+                </div>
+                <div className="textContainer">
+                    <h1 className='postTitle'>Lorem ipsum dolor sit amet.</h1>
+                    <p className='postDesc'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum unde dolor maiores. Ea ducimus quos dicta aperiam laudantium, praesentium dolor unde voluptatibus necessitatibus tenetur porro ipsum perspiciatis aut enim alias.
+                    </p>
+                    <button className='button'>Read more</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
