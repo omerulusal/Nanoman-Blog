945a0ecfc1baee9b859392f38b3ec1123ba5bbec
Ömer Ulusal <96357374+omerulusal@users.noreply.github.com>
2024-01-02 22:31:28 +0300

    .

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 
 
 // GET SINGLE POST
-export const GET = async (req, { params }) => {
+export const GET = async ({ params }) => {
 
-    const { slug } = params
+    const { slug } = params;
 
     try {
         const post = await prisma.post.findUnique({
             where: { slug },
-            include: {user: true},
+            include: { user: true },
         })
         return new NextResponse(JSON.stringify(post, { status: 200 }));
     } catch (error) {
--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -2,12 +2,13 @@ import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
-
     const { searchParams } = new URL(req.url);
+    // Gelen HTTP GET isteğinin URL'sinden parametreleri alır
     const page = searchParams.get('page');
     const cat = searchParams.get('cat');
 
     const POST_PER_PAGE = 2;
+    // Sayfa başına gösterilecek blog yazısı sayısı
 
     const query = {
         take: POST_PER_PAGE,
@@ -19,9 +20,10 @@ export const GET = async (req) => {
 
 
     try {
+        // Prisma $transaction ile iki ayrı sorguyu birleştirip çalıştırır
         const [posts, count] = await prisma.$transaction([
-            prisma.post.findMany(query),
-            prisma.post.count({where:query.where})
+            prisma.post.findMany(query), // Belirtilen kriterlere göre blog yazılarını getirir
+            prisma.post.count({ where: query.where })// Belirtilen kriterlere göre toplam blog yazısı sayısını getirir
         ])
         return new NextResponse(
             JSON.stringify({ posts, count }), { status: 200 }
@@ -39,7 +41,8 @@ export const GET = async (req) => {
 // CREATE A POST
 export const POST = async (req) => {
 
-    const session =  await getAuthSession()
+    const session = await getAuthSession()
+    // kullanıcın oturum durumunu kontrol eder
     if (!session) {
         return new NextResponse(
             JSON.stringify({ error: "Not Authorized" }), { status: 401 }
@@ -47,8 +50,10 @@ export const POST = async (req) => {
     }
     try {
         const body = await req.json();
+        // HTTP POST isteğinin gövdesinden veriyi alıp body degiskenine atanır
         const post = await prisma.post.create({
             data: { ...body, userEmail: session.user.email },
+            // Prisma kullanarak, yeni bir post oluşturur
         })
         return new NextResponse(JSON.stringify(post, { status: 201 }));
     } catch (error) {
